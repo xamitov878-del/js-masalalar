5a0e0b2e3d288c94b4d2fb86acbe03340b89dad2
xamitov878-del <xamitov878@gmail.com>
2026-03-08 14:59:17 +0500

    masalalar 7 tasi tayor

---
+++ script.js
@@ -0,0 +1,65 @@
+// =================================1========================================
+
+// =======1=======
+
+// function sonlar (son){
+//     return son.reverse();
+// }
+
+// console.log(sonlar([1, 2, 3, 4]));
+
+// ======2=======
+
+// function sonlar (son){
+//     return son.join("");
+// }
+
+// console.log(sonlar([1, 2, 3, 4]));
+
+// ======3=======
+
+// function topgich (topilgan, a){
+//     return topilgan.indexOf(a);
+// }
+
+// console.log(topgich([1, 2, 5, 9], 5));
+// console.log(topgich(["a", "b", "c", "d"], "h"));
+
+// ======4=======
+
+// function type (nomi){
+//     return nomi.map(c => typeof c)
+// }
+
+// console.log(type([1, 'a', true, undefined, null]));
+
+// ======5=======
+
+// function g(d) {
+//     return d.map(t => t * 2);
+// }
+
+// console.log(g([1, 2, 3]));
+
+// ======6=======
+
+// function getNumbers(a) {
+//     let result = [];
+//     for (let i = a[0] + 1; i < a[1]; i++) {
+//         result.push(i);
+//     }
+//     return result;
+// }
+
+// console.log(getNumbers([1, 6]));
+
+// ======7=======
+
+// function number(a) {
+//     return (a[0] + a[1]) < 100;
+// }
+
+// console.log(number([10, 57])); 
+// console.log(number([60, 50])); 
+
+// ===============================================================
